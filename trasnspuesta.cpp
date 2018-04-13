ec6ad0658532407b9af955fb94db9f6bb144bb2f
yporrasr <36495526+yporrasr@users.noreply.github.com>
2018-04-13 15:15:30 +0200

    trnaspuesta con 1 dimension

--- trasnspuesta.cpp
+++ trasnspuesta.cpp
@@ -7,19 +7,19 @@ const int N=5;
 
 int main (void)
 {
-  double A[M][N]={{0.0}};// A=matriz original M= filas N=columnas//inicializan en 0.0 
-  double AT[N][M]={{0.0}};//transpuesta (doble corchete por ser arreglo bidimensional
+  double A[M*N]={0.0};// A=matriz original M= filas N=columnas//inicializan en 0.0 
+  double AT[N*M]={0.0};//transpuesta (doble corchete por ser arreglo bidimensional
 
   //para llenar la matriz
   for(int ii=0; ii < M; ++ii){
     for(int jj=0;jj < N; ++jj){ //for dentro de otro for por ser arreglo bidimesional 
-      A[ii][jj]=ii*N  + jj;
+      A[ii*N+jj]=ii*N  + jj;
     }
   }
   //matriz transpuesta
   for(int ii=0; ii<M;++ii){
     for(int jj=0;jj<N;++jj){
-      AT[jj][ii]=A[ii][jj];
+      AT[jj*M+ii]=A[ii*N+jj];
     }
   }
 
@@ -27,7 +27,7 @@ int main (void)
   std::cout<< "matriz original : \n";
   for (int ii=0; ii<M;++ii){
     for(int jj=0; jj<N; ++jj){
-      std::cout <<A[ii][jj]<<"  ";
+      std::cout <<A[ii*N+jj]<<"  ";
     }
     std::cout<<"\n";
       }
@@ -36,7 +36,7 @@ int main (void)
   std::cout<<"matriz transpuesta =\n";
   for (int ii=0; ii<N; ++ii){
     for(int jj=0; jj<M;++jj){
-      std::cout<<AT[ii][jj]<<" ";
+      std::cout<<AT[ii*M+jj]<<" ";
     }
     std::cout<<"\n";
   }
