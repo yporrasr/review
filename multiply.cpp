7a48861eeededc8adc47fc8615da389678f96e69
yporrasr <36495526+yporrasr@users.noreply.github.com>
2018-04-25 08:46:17 -0500

    MATH LIBRARIES

--- multiply.cpp
+++ multiply.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-  Eigen::MatrixXd m = Eigen::MatrixXd::Random(3,3);
+  Eigen::MatrixXd m = Eigen::MatrixXd::Random(3,3);//d=double
   m = (m + Eigen::MatrixXd::Constant(3,3,1.2)) * 50;
   std::cout << "m =" << std::endl << m << std::endl;
-  Eigen::VectorXd v(3);
+  Eigen::VectorXd v(3);//vector de tamaño 3 
   v << 1, 2, 3;
-  std::cout << "m * v =" << std::endl << m * v << std::endl;
+  std::cout << "m * v =" << std::endl << m * v << std::endl;//multiplicacion matriz por vector  // m--> matriz original 
 }
